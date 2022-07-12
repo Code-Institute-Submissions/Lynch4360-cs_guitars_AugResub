5c3b91dc865b6e7f008a8eb14334171d534180d7
Lynch4360 <lynch4360@gmail.com>
2022-07-12 11:25:28 +0000

    Add category filtering functionality to navbar

--- products/views.py
+++ products/views.py
@@ -1,27 +1,36 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 def all_products(request):
     """ A view to display all products to the user, including queries """
 
     products = Product.objects.all()
     query = None
+    categories = None
+
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query) # queries if it is in name OR description
             products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
