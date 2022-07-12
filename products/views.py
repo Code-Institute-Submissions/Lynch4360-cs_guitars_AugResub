af6990d6c59358705b24350fc29e40c5f845e268
Lynch4360 <lynch4360@gmail.com>
2022-07-12 11:35:43 +0000

    added sorting functionality to the main nav

--- products/views.py
+++ products/views.py
@@ -9,14 +9,27 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
-
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -26,6 +39,8 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query) # queries if it is in name OR description
             products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
