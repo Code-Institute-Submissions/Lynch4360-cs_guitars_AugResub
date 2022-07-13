3203678d520ac674b6fa54aa14cc0e0f1d2cb1ef
Lynch4360 <lynch4360@gmail.com>
2022-07-13 10:43:46 +0000

    Added lefty variation to model and variation selector box to productand cart  template

--- products/models.py
+++ products/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
+    in_lefty = models.BooleanField(default=False, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.FloatField(max_length=6)
