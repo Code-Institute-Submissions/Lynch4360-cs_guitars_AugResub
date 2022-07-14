93d156baf0be92063d9cd2fb5be6491dadb39a7a
Lynch4360 <lynch4360@gmail.com>
2022-07-14 13:59:05 +0000

    Added the view and url for the edit and remove functionality in the shopping cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_cart(request):
@@ -33,3 +33,53 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def edit_cart(request, item_id):
+    """ Edit the quantity of a spedcific product in the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    variation = None
+    if 'product_variation' in request.POST:
+        variation = request.POST['product_variation']
+    cart = request.session.get('cart', {})
+
+    if variation:
+        if quantity > 0:
+            cart[item_id]['items_by_variation'][variation] = quantity
+        else:
+            del cart[item_id]['items_by_variation'][variation]
+            if not cart[item_id]['items_by_variation']:
+                cart.pop(item_id)
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """ Edit the quantity of a spedcific product in the shopping cart """
+
+    try:
+        variation = None
+        if 'product_variation' in request.POST:
+            variation = request.POST['product_variation']
+        cart = request.session.get('cart', {})
+
+        if variation:
+            del cart[item_id]['items_by_variation'][variation]
+            if not cart[item_id]['items_by_variation']:
+                cart.pop(item_id)
+        else:
+            cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
+
