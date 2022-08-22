20f010e5df0d12dda05009a89873b31f026dd96b
Lynch4360 <lynch4360@gmail.com>
2022-08-22 23:55:34 +0000

    Deployed to Heroku successfully

--- cs_guitars/settings.py
+++ cs_guitars/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5in(9mhs8!9r71t)%7=rsd@=plij===s1vdiswgfv8z6q@+13)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['csguitars.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,13 +120,17 @@ WSGI_APPLICATION = 'cs_guitars.wsgi.application'
 
 # Database
 # https: // docs.djangoproject.com/en/3.2/ref/settings/  # databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
