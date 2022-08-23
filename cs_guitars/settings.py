32d4f87b413a595ef793d5642290c38d6bdac12a
Lynch4360 <lynch4360@gmail.com>
2022-08-23 00:05:09 +0000

    Removed secret key and set debug

--- cs_guitars/settings.py
+++ cs_guitars/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5in(9mhs8!9r71t)%7=rsd@=plij===s1vdiswgfv8z6q@+13)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['csguitars.herokuapp.com', 'localhost']
 
